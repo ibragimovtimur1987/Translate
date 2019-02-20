a1a2dcd6dd1e92aecb3ef396ceeccb3c8c394f72
Timur <ibragimovtimur@mail.ru>
2019-02-20 21:51:29 +0300

    рефакторинг

--- Translate/Program.cs
+++ Translate/Program.cs
@@ -21,10 +21,10 @@ namespace Translate
             IInputData inputData = ninjectKernel.Get<IInputData>();
             IDictionaryWords dictionaryWords = ninjectKernel.Get<IDictionaryWords>();
             IReplaceWordsService replaceWordsService = ninjectKernel.Get<IReplaceWordsService>();
-            Console.WriteLine(String.Format("Начало программы " + DateTime.Now.ToLongTimeString()));
+            Console.WriteLine(String.Format("Начало программы текущее время {0}",DateTime.Now.ToLongTimeString()));
             TranslateWordsService translateWords = new TranslateWordsService(inputData, dictionaryWords, replaceWordsService);
             List<StringBuilder> result = translateWords.Translate();
-            Console.WriteLine(String.Format("Окончание программы {0} было заменено строк {1}",DateTime.Now.ToLongTimeString(), result.Count));
+            Console.WriteLine(String.Format("Окончание программы текущее время {0} количество строк, где были произведены замены символов равно {1}", DateTime.Now.ToLongTimeString(), result.Count));
             Console.ReadKey();
         }
     }
--- Translate/Services/TranslateWordsService.cs
+++ Translate/Services/TranslateWordsService.cs
@@ -30,30 +30,13 @@ namespace Translate.Service
                 tree.FillTreeNode(data, 0);
                 bool isReplaced = iReplaceWordsService.Replace(data, tree);
                 if(isReplaced) outPutResult.Add(data);
-                if (i == 5)  Console.WriteLine("Строка номер {0} {1}",i ,DateTime.Now.ToLongTimeString());
-                if (i == 10) Console.WriteLine("Строка номер {0} {1}",i ,DateTime.Now.ToLongTimeString());
-                if (i ==20 ) Console.WriteLine("Строка номер {0} {1}",i ,DateTime.Now.ToLongTimeString());
-                if (i == 50) Console.WriteLine("Строка номер {0} {1}",i ,DateTime.Now.ToLongTimeString());
-                if (i == 100)Console.WriteLine("Строка номер {0} {1}", i, DateTime.Now.ToLongTimeString());
+                if (i == 5)  Console.WriteLine("Обработано {0} строк, текущее время {1}",i ,DateTime.Now.ToLongTimeString());
+                if (i == 10) Console.WriteLine("Обработано {0} строк, текущее время {1}",i ,DateTime.Now.ToLongTimeString());
+                if (i ==20 ) Console.WriteLine("Обработано {0} строк, текущее время {1}",i ,DateTime.Now.ToLongTimeString());
+                if (i == 50) Console.WriteLine("Обработано {0} строк, текущее время {1}",i ,DateTime.Now.ToLongTimeString());
+                if (i == 100)Console.WriteLine("Обработано {0} строк, текущее время {1}", i, DateTime.Now.ToLongTimeString());
             }
             return outPutResult;
         }
-
-       
-        //private bool CheckForAdd(List<TreeNode<int, string>> result, TreeNode<int, string> treeNode)
-        //{
-        //    foreach (var treenode in result)
-        //    {
-        //        foreach (var p in treenode.FindPositions)
-        //        {
-        //            int min = p.Key;
-        //            int max = p.Value;
-
-        //            if (treeNode.FindPositions. < min && treeNode.Value > max)
-        //            {
-        //            }
-        //        }
-        //    }
-        //}
     }
 }
